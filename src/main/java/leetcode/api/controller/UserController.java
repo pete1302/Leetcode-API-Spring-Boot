0e1762ad3c9e38d51046e5aa71fa45a290561648
pete1302 <63489114+pete1302@users.noreply.github.com>
2022-07-15 16:09:19 +0530

    base url handling

--- src/main/java/leetcode/api/controller/UserController.java
+++ src/main/java/leetcode/api/controller/UserController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import leetcode.api.service.jsonService;
+import leetcode.api.service.serviceInfo;
 
 // impotr 
 
@@ -21,22 +23,37 @@ public class UserController {
 
     @GetMapping(value ={"/{username}/{qid}", "/"})
     // public ResponseEntity<JSONObject> getStats(@PathVariable Optional<String> username , @PathVariable long qid) {
-    public String getStats(@PathVariable Optional<String> username , @PathVariable int qid) {
-        if (username.isPresent()) {
+    // public String getStats(@PathVariable Optional<String> username , @PathVariable int qid) {
+    public String getStats(@PathVariable(required = false) String username , @PathVariable(required = false) Integer qid) {
+        if (username != null && qid != null) {
             // StatsServiceImpl service = new StatsServiceImpl();
             // return statsService.getStats(username.get());
-            System.out.println(username.get() +' '+ qid);
-            String datastr =  username.get() + String.valueOf(qid);
+            // System.out.println(username.get() +' '+ qid);
+            // String datastr =  username.get() + String.valueOf(qid);
+
+            // JSONObject data = new JSONObject();
+            // data.put("username", username.get());
+            // data.put("qid", qid);
+            // System.out.println(data);
+            // // return new ResponseEntity<JSONObject>(data, HttpStatus.OK);
+            // // return username.get();
+            // // return data.toString();
+            // JSONObject qdata  = new JSONObject();
+            // qdata = jsonService.getJSON(username.get() , qid);
+
+            System.out.println(username +' '+ qid);
+            String datastr =  username + String.valueOf(qid);
 
             JSONObject data = new JSONObject();
-            data.put("username", username.get());
+            data.put("username", username);
             data.put("qid", qid);
             System.out.println(data);
             // return new ResponseEntity<JSONObject>(data, HttpStatus.OK);
             // return username.get();
             // return data.toString();
             JSONObject qdata  = new JSONObject();
-            qdata = jsonService.getJSON(username.get() , qid);
+            qdata = jsonService.getJSON(username , qid);
+
 
             return qdata.toString();
             
@@ -44,13 +61,27 @@ public class UserController {
             // String status = "error";
             // String msg = "please enter your username (ex: leetcode-stats-api.herokuapp.com/LeetCodeUsername)";
             // return StatsResponse.error(status, msg);
-            System.out.println("ERROR1");
-            JSONObject errmsg = new JSONObject();
-            errmsg.put("error", JSONObject.NULL);
+            // System.out.println("ERROR1");
+            // JSONObject errmsg = new JSONObject();
+            // errmsg.put("error", JSONObject.NULL);
+
+            String infoStr = serviceInfo.getServiceInfo();
             // return new ResponseEntity<JSONObject>(errmsg, HttpStatus.OK);
             // return new String();
-            return errmsg.toString();
+            // return errmsg.toString();
+            return infoStr;
         }
     }
+    // @RequestMapping("/")
+    // public String getInfo(){
+        
+    //     return serviceInfo.getServiceInfo();
+    // }
+    // @GetMapping(value = {"/" , "/"})
+    // public String getInfo(){
+        
+    //     return serviceInfo.getServiceInfo();
+    //     // return new String();
+    // }
 }
 
---
+++ src/main/java/leetcode/api/service/serviceInfo.java
@@ -0,0 +1,10 @@
+package leetcode.api.service;
+
+public class serviceInfo {
+    public static String getServiceInfo(){
+        
+        String info = "LEETCODE API  QUERY METHOD -  username/queryId  queryId -> 1 -> 4";
+        // return new String();
+        return info;
+    }
+}
